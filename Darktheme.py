59e30c3a4affbd7f27b6d2874fdf59313ed7031f
saba raheem <104389290+rsaba5612@users.noreply.github.com>
2023-03-15 10:15:50 -0700

    Create Darktheme.py

---
+++ Darktheme.py
@@ -0,0 +1,29 @@
+function main() {
+    var themeNumber = parseInt(readLine(), 10)
+    
+    /*
+    1 - Light
+    2 - Dark
+    3 - Nocturne
+    4 - Terminal
+    5 - Indigo
+    */
+    // Your code here
+    switch(themeNumber){
+        case 1:
+        console.log("Light")
+        break 
+        case 2:
+        console.log("Dark")
+        break 
+        case 3:
+        console.log("Nocturne")
+        break
+case 4:
+console.log("Terminal")
+break 
+case 5:
+console.log ("Indigo")
+break 
+}
+}
