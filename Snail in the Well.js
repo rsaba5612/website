cbba04ad2c9c728c9719646d944da33c39d534b8
saba raheem <104389290+rsaba5612@users.noreply.github.com>
2023-03-15 10:04:38 -0700

    Create Snail in the Well.js

---
+++ Snail in the Well.js
@@ -0,0 +1,15 @@
+function main() {
+    var depth = parseInt(readLine(), 10);
+    //your code goes here
+        i = 0;
+   
+    for (; depth > 0;) {
+        i++;
+       
+        depth -= 7
+        if (depth > 0) {
+            depth += 2
+        }
+    }
+    console.log(i)
+}
