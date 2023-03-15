3cdfb1d212de095a9fa650eada92d6b855e018b4
saba raheem <104389290+rsaba5612@users.noreply.github.com>
2023-03-15 10:05:31 -0700

    Create Currency Converter.js

---
+++ Currency Converter.js
@@ -0,0 +1,10 @@
+function main() {
+    var amount = parseFloat(readLine(), 10);
+    var rate = parseFloat(readLine(), 10);
+
+    function convert(amount, rate) {
+        return amount*rate;
+    }
+    
+    console.log(convert(amount, rate));
+}
