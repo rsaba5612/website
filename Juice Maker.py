4419faac2ac0b81e452fc67d29530a5acd62075f
saba raheem <104389290+rsaba5612@users.noreply.github.com>
2023-03-15 09:38:36 -0700

    Create Juice Maker.py

---
+++ Juice Maker.py
@@ -0,0 +1,14 @@
+class Juice:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def __str__(self):
+        return (self.name + ' ('+str(self.capacity)+'L)')
+
+
+a = Juice('Orange', 1.5)
+b = Juice('Apple', 2.0)
+
+result = a + b
+print(result)
